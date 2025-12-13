a72632d5ab4e0f7c3d0a7d38a756edf9de8b8ad2
Agus76677 <95744231+Agus76677@users.noreply.github.com>
2025-12-13 16:52:17 +0800

    Refactor permutation networks with reusable modules

--- Multlane_RENTT.srcs/sources_1/RTL/auxiliary.v
+++ Multlane_RENTT.srcs/sources_1/RTL/auxiliary.v
@@ -39,37 +39,40 @@ endmodule
 
 
 module shift#(
-	parameter SHIFT = 0, 
-	parameter data_width=12
+        parameter SHIFT = 0,
+        parameter data_width=12
 )
-(	
-	input         clk,
-	input         rst,
+(
+        input         clk,
+        input         rst,
     input  [data_width-1:0] din,
     output [data_width-1:0] dout
 );
 
-reg [data_width-1:0] shift_array [SHIFT-1:0];
-
-always @(posedge clk)begin
-    if(rst)
-        shift_array[0] <= 0;
-    else
-        shift_array[0] <= din;
-end
-
-genvar shft;
-
 generate
-    for(shft=0; shft < SHIFT-1; shft=shft+1) begin: DELAY_BLOCK
-        always @(posedge clk ) begin
+    if (SHIFT == 0) begin : NO_SHIFT
+        assign dout = din;
+    end else begin : SHIFT_CHAIN
+        reg [data_width-1:0] shift_array [0:SHIFT-1];
+        genvar shft;
+
+        always @(posedge clk) begin
             if(rst)
-                shift_array[shft+1] <= 0;
+                shift_array[0] <= 0;
             else
-                shift_array[shft+1] <= shift_array[shft];
+                shift_array[0] <= din;
         end
+
+        for(shft=0; shft < SHIFT-1; shft=shft+1) begin: DELAY_BLOCK
+            always @(posedge clk ) begin
+                if(rst)
+                    shift_array[shft+1] <= 0;
+                else
+                    shift_array[shft+1] <= shift_array[shft];
+            end
+        end
+
+        assign dout = shift_array[SHIFT-1];
     end
 endgenerate
-
-assign dout = shift_array[SHIFT-1];
 endmodule
