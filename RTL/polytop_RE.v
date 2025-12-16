ae1a50d18b55b4d1a253f096e3d4810b2cf3341a
Agus76677 <95744231+Agus76677@users.noreply.github.com>
2025-12-16 19:57:23 +0800

    Revert "Add optional Benes scatter fabric"

--- RTL/polytop_RE.v
+++ RTL/polytop_RE.v
@@ -83,6 +83,7 @@ wire [`DATA_WIDTH-1:0] wa [0:`P-1];
 //tf address for ROM
 wire [`ADDR_ROM_WIDTH-1:0] tf_address;
 
+(*DONT_TOUCH = "true"*)
 fsm fsm_inst(
     .clk                               (clk                       ),//I
     .rst                               (rst                       ),//I
@@ -99,6 +100,7 @@ fsm fsm_inst(
 genvar i_AG;
 generate
     for(i_AG = 0; i_AG < `P_HALF; i_AG = i_AG + 1) begin : gen_addr_gen
+        (*DONT_TOUCH = "true"*)
         addr_gen addr_gen_inst(
             .clk                       (clk                       ),//I
             .rst                       (rst                       ),//I
@@ -112,6 +114,7 @@ generate
             .io1                       (old_io1[i_AG]             ) //O
         );
 
+        (*DONT_TOUCH = "true"*)
         memory_map memory_map_inst(
             .clk                       (clk                       ),//I
             .rst                       (rst                       ),//I
@@ -145,14 +148,16 @@ generate
     end
 endgenerate
 
+(*DONT_TOUCH = "true"*)
 arbiter m3(
     .BI_bus                            (BI_bus                    ),//I
     .sel_BI_bus                        (sel_BI_bus                ) //0
 );
-
+                      
+(*DONT_TOUCH = "true"*)
 network_bank_in mux1(
     .BA_bus                            (BA_bus                     ),//I
-    .BI_bus                            (BI_bus                     ),//I
+    .sel_BI_bus                        (sel_BI_bus                 ),//I
     .new_address_bus                   (new_address_bus            ) //O
 );
 
@@ -162,6 +167,7 @@ generate
     for(i_dff = 0; i_dff < 2*`P; i_dff = i_dff + 1) begin : gen_dff
         shift#(.SHIFT(`L+1),.data_width(8)) dff_n0(.clk(clk),.rst(rst),.din(new_address[i_dff]),.dout(new_address_ff[i_dff]));
         
+        (*DONT_TOUCH = "true"*)
         bank bank_inst(
             .clk                               (clk                       ),//I
             .waddr                             (new_address_ff[i_dff]     ),//I
@@ -181,6 +187,7 @@ generate
 endgenerate
 
 
+(*DONT_TOUCH = "true"*)
 network_RBFU_in dmux(
     .clk                               (clk                       ),//I
     .rst                               (rst                       ),//I
@@ -193,6 +200,7 @@ genvar i_rbfu;
 generate
     for(i_rbfu = 0; i_rbfu < `P_HALF ; i_rbfu = i_rbfu + 1) begin : gen_rbfu
         //BFU
+        (*DONT_TOUCH = "true"*)
         RBFU u_RBFU(
             .clk                               (clk                       ),
             .rst                               (rst                       ),
@@ -222,14 +230,16 @@ generate
     end
 endgenerate 
 
+(*DONT_TOUCH = "true"*)          
 network_RBFU_out mux3(
     .clk                               (clk                            ),//I
     .rst                               (rst                            ),//I
     .bf_out_bus                        (bf_out_bus                     ),//I
-    .BI_bus                            (BI_bus                         ),//I
+    .sel_BI_bus                        (sel_BI_bus                     ),//I
     .d_in_bus                          (d_in_bus                       ) //O
 );
 
+(*DONT_TOUCH = "true"*)
 tf_address_generator m6(
     .clk                               (clk                            ),//I
     .rst                               (rst                            ),//I
@@ -239,6 +249,7 @@ tf_address_generator m6(
     .tf_address                        (tf_address                     ) //O
 );
 
+(*DONT_TOUCH = "true"*)
 tf_ROM rom0(
     .clk                               (clk                           ),//I
     .A                                 (tf_address                    ),//I
