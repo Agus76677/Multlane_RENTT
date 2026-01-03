eb11cd2aec6af9171764d83b980b21b74b5cc89c
Agus76677 <766776054@qq.com>
2026-01-03 11:21:01 +0800

    update

--- RPMA/basic_unit/Modmul.v
+++ RPMA/basic_unit/Modmul.v
@@ -35,58 +35,6 @@ bitmod_wocsa3 u_bitmod_wocsa3(
     .R                                 (R                         ) 
 );
 endmodule
-`elsif PIPE2
-module Modmul(
-            input         clk,
-            input  [11:0] A,B,
-            output [11:0] R);
 
-wire [23:0] P;
-
-intmul u_intmul(A,B,P);
-// ---------------------------------------
-bitmod_wocsa3 u_bitmod_wocsa3(
-    .clk                               (clk                       ),
-    .C                                 (P                         ),
-    .R                                 (R                         ) 
-);
-endmodule
-`elsif PIPE3
-module Modmul(
-            input         clk,
-            input  [11:0] A,B,
-            output [11:0] R);
-
-wire [23:0] P;
-
-intmul u_intmul(A,B,P);
-// ---------------------------------------
-bitmod_wocsa3 u_bitmod_wocsa3(
-    .clk                               (clk                       ),
-    .C                                 (P                         ),
-    .R                                 (R                         ) 
-);
-endmodule
-`else
-module Modmul(
-            input         clk,
-            input  [11:0] A,B,
-            output [11:0] R);
-
-wire [23:0] P;
-reg  [23:0] P_ff1=0;
-
-intmul u_intmul(A,B,P);
-
-always @(posedge clk) begin
-        P_ff1 <= P;
-end
-// ---------------------------------------
-bitmod_wocsa3 u_bitmod_wocsa3(
-    .clk                               (clk                       ),
-    .C                                 (P_ff1                     ),
-    .R                                 (R                         ) 
-);
-endmodule
 
 `endif
